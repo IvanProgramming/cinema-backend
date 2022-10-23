1ba48699509acc65206fb79b8de6d86c9819d447
Ivan <23701564+IvanProgramming@users.noreply.github.com>
2022-10-24 00:47:50 +0300

    Periodical fetch added

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import { Hono } from 'hono'
 import { Bindings } from 'hono/dist/hono'
 import { NotFoundError, OkResponse } from './responses'
 import { Env } from './env'
-import { getFilms, getFilm } from './notion'
+import { getFilms, getFilm, NotionApiClient } from './notion'
 const app = new Hono<{Bindings: Env}>()
 
 app.get('/ping', (c) => (new OkResponse({pong: 'pong'})).asJsonResponse(c))
 app.get('/films', getFilms)
 app.get('/film/:id', getFilm)
 app.all('*', (c) => (new NotFoundError().asJsonResponse(c)))
-export default app
+export default {
+    fetcher: app.fetch,
+    scheduled: async (event: ScheduledEvent, env: Env) => {
+        let client = new NotionApiClient(env.NOTION_TOKEN)
+        let data = await client.fetchDatabase(env.NOTION_DATABASE_ID)
+        await env.KV.put("filmsDb", JSON.stringify(data))
+    }
+}
