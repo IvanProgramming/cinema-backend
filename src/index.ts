aa6f4a8400e76a417418912718e6a57f33bf54c4
Ivan <23701564+IvanProgramming@users.noreply.github.com>
2022-10-27 21:30:31 +0300

    Added cors header and fixed ISE

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import { Hono } from 'hono'
-import { Bindings } from 'hono/dist/hono'
+import { cors } from 'hono/cors'
 import { NotFoundError, OkResponse } from './responses'
 import { Env } from './env'
 import { getFilms, getFilm, NotionApiClient } from './notion'
 const app = new Hono<{Bindings: Env}>()
+
+app.use("*", cors({
+    origin: ['http://localhost:3000', 'https://cinema.ivanisplaying.xyz'],
+    allowMethods: ["GET", "OPTIONS"]
+}))
 
 app.get('/ping', (c) => (new OkResponse({pong: 'pong'})).asJsonResponse(c))
 app.get('/films', getFilms)
 app.get('/film/:id', getFilm)
 app.all('*', (c) => (new NotFoundError().asJsonResponse(c)))
 export default {
-    fetcher: app.fetch,
+    fetch: app.fetch,
     scheduled: async (event: ScheduledEvent, env: Env) => {
         let client = new NotionApiClient(env.NOTION_TOKEN)
         let data = await client.fetchDatabase(env.NOTION_DATABASE_ID)
